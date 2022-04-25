f13c588455f3b4cab0c71d98c5d208270e47e978
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-25 10:22:59 +0530

    calculate wage

--- Employee.wage/Employee.wage/Program.cs
+++ Employee.wage/Employee.wage/Program.cs
@@ -2,34 +2,51 @@
 Console.WriteLine("Welcome to Employee Wage!");
 int employeewageprhr = 20;
 int employeeworkinghr = 0;
-int employeeworkingdays = 20;
+int employeeworkingdays = 0;
+int maximumworkinghr = 100;
+int maximumworkingdays = 20;
 Random emp = new Random();
 int isPresent = emp.Next(0, 4);
 Console.WriteLine(isPresent);
-
-
-switch (isPresent)
+if (isPresent == 1)
 {
-    case 1:
-        Console.WriteLine("Employee is Present and FullTime!");
-        employeeworkinghr = 8;
-        break;
-    case 2:
-        Console.WriteLine("Employee is Present and PartTime!");
-        employeeworkinghr = 4;
-        break;
-    case 0:
-        Console.WriteLine("Employee is Absent!");
-        employeeworkinghr = 0;
-        break;
-    default:
-        Console.WriteLine("wrong input!");
-        break;
-
+    Console.WriteLine("Employee is present!");
+    Console.WriteLine("Employee is Full Time");
 }
-int EmployeeWage = employeewageprhr * employeeworkinghr;
-int EmployeeMonthlysalary = EmployeeWage * employeeworkingdays;
-Console.WriteLine("Employee Daily Wage = " + EmployeeWage);
-Console.WriteLine("Employee Monthly Wage = " + EmployeeMonthlysalary);
+else if (isPresent == 2)
+{
+    Console.WriteLine("Employee is Present!");
+    Console.WriteLine("Employee is Part Time");
+}
+
+else
+{
+    Console.WriteLine("Employee is Absent!");
+}
+while ((employeeworkinghr < maximumworkinghr) && (employeeworkingdays < maximumworkingdays))
+{
+
+    switch (isPresent)
+    {
+        case 1:
+            employeeworkinghr += 8;
+            break;
+        case 2:
+            employeeworkinghr += 4;
+            break;
+        case 0:
+            employeeworkinghr = 0;
+            break;
+        default:
+            break;
+
+    }
+    employeeworkingdays++;
+
+    Console.WriteLine("Total Working Hours : " + employeeworkinghr);
+    Console.WriteLine("Total working Days Per Month : " + employeeworkingdays);
+    int totalSalary = employeewageprhr * employeeworkinghr;
+    Console.WriteLine("Total Salary of Employee per Month : " + totalSalary);
+}
 
 
