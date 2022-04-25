7fd416c4e30f108ae5ae1e1c76f90c6c3d4d2c7e
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-25 10:10:24 +0530

    Daily Wage

--- Employee.wage/Employee.wage/Program.cs
+++ Employee.wage/Employee.wage/Program.cs
@@ -1,9 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage!");
+Console.WriteLine("Welcome to Employee wage computation!");
+int employeewageprhr = 20;
+int employeeworkinghr;
 Random emp = new Random();
 int isPresent = emp.Next(0, 2);
 Console.WriteLine(isPresent);
+
+
 if (isPresent == 1)
+{
+    employeeworkinghr = 8;
     Console.WriteLine("Employee is Present");
+}
 else
+{
+    employeeworkinghr = 0;
     Console.WriteLine("Employee is Absent");
+}
+int EmployeeWage = employeewageprhr * employeeworkinghr;
+Console.WriteLine("Employee wage = " + EmployeeWage);
+
